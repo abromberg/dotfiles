f0c1cba0c5cf1df106b7993439c79e5deeba48d6
Andy Bromberg <abromberg@stanford.edu>
2013-06-27 23:33:38 -0700

    added autopair and yasnippet

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -91,3 +91,7 @@
 (add-hook 'python-mode-hook 'auto-complete-mode)
 (add-hook 'python-mode-hook 'jedi:ac-setup)
 (add-hook 'after-init-hook #'global-flycheck-mode)
+(require 'autopair)
+(autopair-global-mode) ;; to enable in all buffers
+(require 'yasnippet)
+(yas-global-mode 1)
